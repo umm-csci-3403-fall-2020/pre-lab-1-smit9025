10d2b7142cd42dfabbb4e1229a508edd4229073b
root <root@FriggOff.localdomain>
2020-08-31 01:24:32 -0500

    Fix variables in wrap_contents.sh Fixed the script so that variables where actually created within wrap_contents.sh

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FILE = "$1" #Store first argument which is the contents file
-WRAP = "$2" #Store second argument which is the name of desired header and footer
-HTML = "$3" #Store third argument which is the resulting file
+FILE="$1" #Store first argument which is the contents file
+WRAP="$2" #Store second argument which is the name of desired header and footer
+HTML="$3" #Store third argument which is the resulting file
 
 cat "$WRAP"_header.html "$FILE" "$WRAP"_footer.html >> "$HTML" #Use cat to concatenate files together and then sends to HTML output file
